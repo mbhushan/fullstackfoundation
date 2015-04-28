6126b95b9f8d03c8ae7c0907c5f4d564089a0855
manibhushan <manibhushan.cs@gmail.com>
2015-04-28 19:47:42 +0530

    added edit functionality

--- webserver.py
+++ webserver.py
@@ -32,6 +32,26 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                print "Restaurant ID PATH: ", restaurantIDPath
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    print "INSIDE MY RESTAURANT ID PATH"
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += myRestaurantQuery.name
+                    output += "</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                    output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
@@ -42,7 +62,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br>"
-                    output += "<a href = '#'>Edit</a>"
+                    output += "<a href = '/restaurants/%s/edit'>Edit</a>" % restaurant.id
                     output += "</br>"
                     output += "<a href = '#'>Delete</a>"
                     output += "</br></br></br>"
@@ -54,6 +74,28 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/edit"):
+                print "inside post EDIT"
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                print "CTYPE: ", ctype
+                if ctype == 'multipart/form-data':
+                    print "inside FORM DATA"
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+                    print "POST restaurantIDPath: ", restaurantIDPath
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    print "my restaurant query name: ", myRestaurantQuery.name
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
                 if ctype == 'multipart/form-data':
