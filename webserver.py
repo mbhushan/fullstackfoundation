f1e3486d1fbf8f13e1d9d5bd727226e1f7f47f76
manibhushan <manibhushan.cs@gmail.com>
2015-04-28 07:34:17 +0530

    webserver for handling http request/response

---
+++ webserver.py
@@ -0,0 +1,32 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ''
+                output += "<html><body>ManiBhushan</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+        except IOError:
+            self.send_error(404, "File Not Found %s" % self.path)
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print "Webserver running on port: " % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "^C entered, stopping webserver..."
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
