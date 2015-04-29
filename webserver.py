b9d475169deebba334109ace16eac12b1bf0cb2d
manibhushan <manibhushan.cs@gmail.com>
2015-04-29 08:39:06 +0530

    added deleting restaurant functionality

--- webserver.py
+++ webserver.py
@@ -32,6 +32,23 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>Are you sure you want to delete %s?" % myRestaurantQuery.name
+                    output += "<form method='POST' enctype = 'multipart/form-data' action = '/restaurants/%s/delete'>" % restaurantIDPath
+                    output += "<input type = 'submit' value = 'Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
             if self.path.endswith("/edit"):
                 restaurantIDPath = self.path.split("/")[2]
                 print "Restaurant ID PATH: ", restaurantIDPath
@@ -64,7 +81,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     output += "</br>"
                     output += "<a href = '/restaurants/%s/edit'>Edit</a>" % restaurant.id
                     output += "</br>"
-                    output += "<a href = '#'>Delete</a>"
+                    output += "<a href = '/restaurants/%s/delete'>Delete</a>" % restaurant.id
                     output += "</br></br></br>"
                 output += "</body></html>"
                 self.wfile.write(output)
@@ -74,6 +91,17 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    session.delete(myRestaurantQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
             if self.path.endswith("/edit"):
                 print "inside post EDIT"
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
