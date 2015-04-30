fbb821247fbcad5d6ff5623d59e088a588688e46
manibhushan <manibhushan.cs@gmail.com>
2015-04-30 16:57:57 +0530

    added flash messages

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -35,6 +35,7 @@ def new_menuitem(restaurant_id):
                            restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurants', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -70,5 +71,6 @@ def delete_menuitem(restaurant_id, menu_id):
         return render_template('deletemenuitem.html', item=itemToDelete)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5002)
