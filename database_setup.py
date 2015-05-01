ef7994ca3917fdec04e65de83e120d9f2352ef2b
manibhushan <manibhushan.cs@gmail.com>
2015-05-01 06:34:44 +0530

    json end points for menu list and menu item

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,3 @@
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -12,6 +11,14 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """ return object data in serializable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -22,6 +29,18 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        """ return object data in serializable format """
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+            'restaurant_id': self.restaurant_id
+        }
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
